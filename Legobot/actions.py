70feb9709a5102d30e34d9c1e6659c23f8f521f2
Gidntsquia <32310846+Gidntsquia@users.noreply.github.com>
2020-03-06 18:37:44 -0500

    Add swing function

--- Legobot/actions.py
+++ Legobot/actions.py
@@ -12,3 +12,12 @@ import gyro as g
 @print_function_name
 def sample_command():
     m.drive(1000)
+
+
+@print_function_name
+def pull_swings():
+    g.drive_gyro(500)
+    msleep(100)
+    g.backwards_gyro(1000)
+
+# Should get 100 points, but need build to be completed first. 
